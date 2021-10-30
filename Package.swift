790f09c6708acaefb97b6f552e832458edaf05a6
David Keegan <git@davidkeegan.com>
2021-10-29 21:11:17 -0700

    Update ivOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [
         .iOS(.v11),
         .macOS(.v10_15),
-        .tvOS(.v11)
+        .tvOS(.v13)
     ],
     products: [
         .library(name: "Looping", targets: ["Looping"]),
